e2a8e1b0734868b843a3f662b684549c626fea59
kphoen <geek63@gmail.com>
2011-07-18 21:15:24 +0200

    Chg: removed old installation scripts

--- git/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "[-][git] Begin installation ..."
-
-# we create symbolic links to the config files if they don't already exist
-[ ! -e ~/.gitconfig ] && ln -s "`pwd`/gitconfig" ~/.gitconfig
-
-echo "[+][git] installation complete."

72688f1efa5c48cfbd74efaa2521efa4f23d8a70
Kévin Gomez Pinto <kevin_gomez@carpe-hora.com>
2011-08-12 15:35:17 +0200

    Add: NERD Commenter for vim

--- vim/_scripts/install.sh
+++ vim/_scripts/install.sh
@@ -12,7 +12,7 @@ echo "[-][vim] Begin installation ..."
 
 # install dependancies
 IFS=";"
-dependancies='syntastic;tagbar;vim-symfony;nerdtree'
+dependancies='syntastic;tagbar;vim-symfony;nerdtree;nerdcommenter'
 for dep_name in $dependancies; do
   # check if the current dependancy is already installed
   if [[ -e "_cache/$dep_name"  ]]; then

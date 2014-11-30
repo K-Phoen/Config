eaf7f43c6771269714e38997b48b5eb578a0fa5d
Kévin Gomez <geek63@gmail.com>
2014-11-30 13:20:05 +0100

    Update vim install script

--- vim/_scripts/install.sh
+++ vim/_scripts/install.sh
@@ -6,21 +6,4 @@ echo "[-][vim] Begin installation ..."
 [ ! -e ~/.vimrc ] && ln -s "`pwd`/vimrc" ~/.vimrc
 [ ! -e ~/.vim ] && ln -s "`pwd`/vim" ~/.vim
 
-
-# ensure that the installation cache directory exists
-[ ! -d _cache/ ] && mkdir _cache
-
-# install dependancies
-IFS=";"
-dependancies='vim-pathogen'
-for dep_name in $dependancies; do
-  # check if the current dependancy is already installed
-  if [[ -e "_cache/$dep_name"  ]]; then
-    echo "[*][vim] $dep_name is already installed: nothing done."
-    continue
-  fi
-
-  ./_scripts/install_dependancy.sh "$dep_name"
-done
-
 echo "[+][vim] installation complete."

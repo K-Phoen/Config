ac6df273aea554c2cc02069aae35ef4cee99d49b
Kévin Gomez <geek63@gmail.com>
2014-11-30 13:17:54 +0100

    Fix bin creation

--- bin/_scripts/install.sh
+++ bin/_scripts/install.sh
@@ -2,6 +2,8 @@
 
 echo "[-][bin] Begin installation ..."
 
+[[ ! -d ~/bin ]] && mkdir ~/bin
+
 for script in `ls`; do
   if [[ $script != "_scripts" ]]; then
     [ ! -e ~/bin/$script ] && ln -s "`pwd`/$script" ~/bin/$script

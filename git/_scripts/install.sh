77dbd12035a19bd5829a7268f780646bf892cc5a
Kévin Gomez <contact@kevingomez.fr>
2015-08-29 22:34:40 +0200

    Add global gitignore

--- git/_scripts/install.sh
+++ git/_scripts/install.sh
@@ -4,5 +4,6 @@ echo "[-][git] Begin installation ..."
 
 # we create symbolic links to the config files if they don't already exist
 [ ! -e ~/.gitconfig ] && ln -s "`pwd`/gitconfig" ~/.gitconfig
+[ ! -e ~/.gitignore.global ] && ln -s "`pwd`/gitignore.global" ~/.gitignore.global
 
 echo "[+][git] installation complete."

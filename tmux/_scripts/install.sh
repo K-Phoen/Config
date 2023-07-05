1c7791a2e46528c5ea2abd9ba1ebee6cafe1fe22
Kévin Gomez <contact@kevingomez.fr>
2023-07-05 12:29:06 +0200

    Remork tmux config

--- tmux/_scripts/install.sh
+++ tmux/_scripts/install.sh
@@ -2,3 +2,5 @@
 
 # we create symbolic links to the config files if they don't already exist
 [ ! -e ~/.tmux.conf ] && ln -s "`pwd`/tmux.conf" ~/.tmux.conf
+
+[ ! -d ~/.tmux/plugins/tpm ] && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm

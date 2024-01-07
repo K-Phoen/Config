f2b879e4095c09ac4f2aa0cc15e5933bd48a8c9d
Kévin Gomez <contact@kevingomez.fr>
2024-01-07 16:49:06 +0100

    update spacevim clone URL

--- vim/_scripts/install.sh
+++ vim/_scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ ! -d ~/.SpaceVim ] && git clone git@github.com:SpaceVim/SpaceVim.git ~/.SpaceVim
+[ ! -d ~/.SpaceVim ] && git clone https://gitlab.com/SpaceVim/SpaceVim.git ~/.SpaceVim
 [ ! -e ~/.SpaceVim.d ] && ln -s "`pwd`/SpaceVim.d" ~/.SpaceVim.d
 [ ! -e ~/.vim ] && ln -s ~/.SpaceVim ~/.vim
 [ ! -e ~/.config/nvim ] && ln -s ~/.SpaceVim ~/.config/nvim

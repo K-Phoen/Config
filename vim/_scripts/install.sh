74d599b7a98eed8176de45083699773e0b5f83a5
Kévin Gomez <kevin.gomez@grafana.com>
2025-04-19 01:22:26 +0200

    try lazyvim

--- vim/_scripts/install.sh
+++ vim/_scripts/install.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-[ ! -d ~/.SpaceVim ] && git clone https://gitlab.com/SpaceVim/SpaceVim.git ~/.SpaceVim
-[ ! -e ~/.SpaceVim.d ] && ln -s "`pwd`/SpaceVim.d" ~/.SpaceVim.d
-[ ! -e ~/.vim ] && ln -s ~/.SpaceVim ~/.vim
-[ ! -e ~/.config/nvim ] && ln -s ~/.SpaceVim ~/.config/nvim
+[ ! -e ~/.config/nvim ] && ln -s "`pwd`/lazyvim" ~/.config/nvim
---
+++ vim/lazyvim/lua/plugins/custom.lua
@@ -0,0 +1,16 @@
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  { "catppuccin/nvim", name = "catppuccin", lazy = false },
+  -- Configure LazyVim to load catppuccin
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-macchiato",
+    },
+  },
+}

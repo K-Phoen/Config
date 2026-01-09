5c143e3742e82229d0b789acd3591b61737b5003
Kévin Gomez <kevin.gomez@grafana.com>
2026-01-09 14:19:51 +0100

    Install statusline

--- vim/nvim/lua/plugins/ui.lua
+++ vim/nvim/lua/plugins/ui.lua
@@ -14,4 +14,22 @@ return {
       },
     },
   },
+  -- Statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    event = "VeryLazy",
+    config = function()
+      require("lualine").setup({
+        sections = {
+          lualine_a = {'mode'},
+          lualine_b = {'diff', 'diagnostics'},
+          lualine_c = {'filename'},
+          lualine_x = {'encoding', 'fileformat', 'filetype'},
+          lualine_y = {'progress'},
+          lualine_z = {'location'}
+        },
+      })
+    end,
+  },
 }

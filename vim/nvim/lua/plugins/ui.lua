585377ea80477ef0ff1ae2e5fe40e62bdf0f47ee
Kévin Gomez <kevin.gomez@grafana.com>
2026-01-09 14:36:17 +0100

    Display indent guide for current scope

--- vim/nvim/lua/plugins/ui.lua
+++ vim/nvim/lua/plugins/ui.lua
@@ -32,4 +32,13 @@ return {
       })
     end,
   },
+  -- Display indent guide for current scope.
+  -- Other option: https://github.com/lukas-reineke/indent-blankline.nvim
+  {
+    'nvim-mini/mini.indentscope',
+    version = '*',
+    config = function()
+      require('mini.indentscope').setup()
+    end
+  },
 }

2a590ab6c2afa686760731c4958d54ca7e5f7710
Kévin Gomez <kevin.gomez@grafana.com>
2026-01-13 00:10:43 +0100

    Remove tabline plugin for now

--- vim/nvim/lua/plugins/ui.lua
+++ vim/nvim/lua/plugins/ui.lua
@@ -241,30 +241,4 @@ return {
       },
     },
   },
-  -- Pretty tab-line
-  {
-    'romgrk/barbar.nvim',
-    dependencies = {
-      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
-      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-    },
-    init = function() vim.g.barbar_auto_setup = false end,
-    opts = {
-      animation = false,
-      sidebar_filetypes = {
-        -- Use the default values: {event = 'BufWinLeave', text = '', align = 'left'}
-        NvimTree = true,
-        -- Or, specify the text used for the offset:
-        undotree = {
-          text = 'undotree',
-          align = 'center', -- *optionally* specify an alignment (either 'left', 'center', or 'right')
-        },
-        -- Or, specify the event which the sidebar executes when leaving:
-        ['neo-tree'] = {event = 'BufWipeout'},
-        -- Or, specify all three
-        Outline = {event = 'BufWinLeave', text = 'symbols-outline', align = 'right'},
-      },
-    },
-    version = '^1.0.0', -- optional: only update when a new 1.x version is released
-  }
 }

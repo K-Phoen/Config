184401e6cc9c7372a752039177154ca22498edcc
Kévin Gomez <kevin.gomez@grafana.com>
2026-01-13 22:51:31 +0100

    Use nvim-mini/mini.icons for icons

--- vim/nvim/lua/plugins/ui.lua
+++ vim/nvim/lua/plugins/ui.lua
@@ -90,6 +90,26 @@ function show_commands()
 end
 
 return {
+  -- Icons
+  {
+    "nvim-mini/mini.icons",
+    lazy = true,
+    opts = {
+      file = {
+        [".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
+        ["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
+      },
+      filetype = {
+        dotenv = { glyph = "", hl = "MiniIconsYellow" },
+      },
+    },
+    init = function()
+      package.preload["nvim-web-devicons"] = function()
+        require("mini.icons").mock_nvim_web_devicons()
+        return package.loaded["nvim-web-devicons"]
+      end
+    end,
+  },
   -- Display keymaps in a popup
   {
     "folke/which-key.nvim",

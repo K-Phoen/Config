2d231f3e610fa8b36be23ec9323a7e22953cc5b1
Kévin Gomez <kevin.gomez@grafana.com>
2026-01-15 23:20:56 +0100

    LSP declarations previews

--- vim/nvim/lua/plugins/ui.lua
+++ vim/nvim/lua/plugins/ui.lua
@@ -247,4 +247,14 @@ return {
       -- Your setup opts here
     },
   },
+  -- Preview LSP's goto definition, type definition, implementation, ... in floating windows.
+  {
+    "rmagatti/goto-preview",
+    dependencies = { "rmagatti/logger.nvim" },
+    event = "BufEnter",
+    config = true, -- necessary as per https://github.com/rmagatti/goto-preview/issues/88
+    opts = {
+      default_mappings = true,
+    },
+  },
 }

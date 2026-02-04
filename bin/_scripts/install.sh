86b77398284c04341abe7cef65b43ac94d6cdbce
Kévin Gomez <kevin.gomez@grafana.com>
2026-02-04 23:30:57 +0100

    remove unused ./bin/ directory

--- bin/_scripts/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-[[ ! -d ~/bin ]] && mkdir ~/bin
-
-for script in `ls`; do
-  if [[ $script != "_scripts" ]]; then
-    [ ! -e ~/bin/$script ] && ln -s "`pwd`/$script" ~/bin/$script
-  fi
-done
